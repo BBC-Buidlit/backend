63a2463829aa0c779d82b1e4b40e3a9e230754b7
Rohan Dhar <rohan19443@iiitd.ac.in>
2021-12-11 06:14:20 +0530

    Add check for existing users

--- src/service/user_service.ts
+++ src/service/user_service.ts
@@ -5,33 +5,41 @@ import { IDiscordUser } from "../models/IDiscord";
 import userModel, { IUser } from "../models/IUser";
 
 class UserService {
-  async getOneById(id: string) {
-    if (!Types.ObjectId.isValid(id))
-      throw new BadRequest(`Invalid user id : ${id}`);
+	async getOneById(id: string) {
+		if (!Types.ObjectId.isValid(id))
+			throw new BadRequest(`Invalid user id : ${id}`);
 
-    const user = await userModel.findById(id);
-    if (!user) throw new EntityNotFound("User not found ${id}", "user");
-    return user;
-  }
+		const user = await userModel.findById(id);
+		if (!user) throw new EntityNotFound("User not found ${id}", "user");
+		return user;
+	}
 
-  async addOne(userPayload: IUser) {
-    try {
-      const user = await userModel.create({ ...userPayload });
-      return user;
-    } catch (err: unknown) {
-      throw new BadRequest(`${err}`);
-    }
-  }
+	async addOne(userPayload: IUser) {
+		const { username } = userPayload;
 
-  async saveFromDiscordUser(accessToken: string, discordUser: IDiscordUser) {
-    const user: IUser = {
-      access_token: accessToken,
-      username: discordUser.username,
-      discord_id: discordUser.id,
-      avatar_id: discordUser.avatar,
-    };
-    return await this.addOne({ ...user });
-  }
+		try {
+			const existingUser = await userModel.findOne({ username });
+			if (existingUser) return existingUser;
+		} catch (err: unknown) {}
+
+		try {
+			const user = await userModel.create({ ...userPayload });
+			return user;
+		} catch (err: unknown) {
+			console.log(err);
+			throw new BadRequest(`${err}`);
+		}
+	}
+
+	async saveFromDiscordUser(accessToken: string, discordUser: IDiscordUser) {
+		const user: IUser = {
+			access_token: accessToken,
+			username: discordUser.username,
+			discord_id: discordUser.id,
+			avatar_id: discordUser.avatar,
+		};
+		return await this.addOne({ ...user });
+	}
 }
 
 export default UserService;
